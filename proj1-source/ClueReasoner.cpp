c3e727d7b8fb7fe03c6882ccc7bfad2bcc40b52a
Patrick Truong <patricct@usc.edu>
2018-09-24 14:01:11 -0700

    Have to fix initial clauses

--- proj1-source/ClueReasoner.cpp
+++ proj1-source/ClueReasoner.cpp
@@ -163,12 +163,12 @@ void ClueReasoner::AddInitialClauses()
 	// No two cards in each category can both be in the case file.
 	//Similar logic to no cards should be in two places, except if there's one card of a category in the case file, then every other card cannot be assigned to the case file
     vector<Clause> oneCardOfCategoryInCaseFile; //A disjunction of a card being in case file
-    for(int s = 0; s <= num_suspects; s++) { //Iterate over all players
+    for(int s = 0; s < num_suspects; s++) {
         vector<Clause> cardInCaseFileConjunction;
         Clause cardInCaseFileClause;
         cardInCaseFileClause.push_back(GetPairNum("cf", suspects[s]));
         cardInCaseFileConjunction.push_back(cardInCaseFileClause);
-        for(int other = 0; other <= num_suspects; other++) {
+        for(int other = 0; other < num_suspects; other++) {
             if(other != s) {
                 Clause cardNotInCaseFile;
                 cardNotInCaseFile.push_back(GetPairNum("cf", suspects[other]) * -1);
@@ -184,14 +184,14 @@ void ClueReasoner::AddInitialClauses()
     for(Clause clause : oneCardOfCategoryInCaseFile) {
         solver->AddClause(clause);
     }
-    oneCardOfCategoryInCaseFile.empty();
+    oneCardOfCategoryInCaseFile.clear();
     
-    for(int w = 0; w <= num_weapons; w++) { //Iterate over all players
+    for(int w = 0; w < num_weapons; w++) {
         vector<Clause> cardInCaseFileConjunction;
         Clause cardInCaseFileClause;
         cardInCaseFileClause.push_back(GetPairNum("cf", weapons[w]));
         cardInCaseFileConjunction.push_back(cardInCaseFileClause);
-        for(int other = 0; other <= num_weapons; other++) {
+        for(int other = 0; other < num_weapons; other++) {
             if(other != w) {
                 Clause cardNotInCaseFile;
                 cardNotInCaseFile.push_back(GetPairNum("cf", weapons[other]) * -1);
@@ -207,14 +207,14 @@ void ClueReasoner::AddInitialClauses()
     for(Clause clause : oneCardOfCategoryInCaseFile) {
         solver->AddClause(clause);
     }
-    oneCardOfCategoryInCaseFile.empty();
+    oneCardOfCategoryInCaseFile.clear();
     
-    for(int r = 0; r <= num_rooms; r++) { //Iterate over all players
+    for(int r = 0; r < num_rooms; r++) {
         vector<Clause> cardInCaseFileConjunction;
         Clause cardInCaseFileClause;
         cardInCaseFileClause.push_back(GetPairNum("cf", rooms[r]));
         cardInCaseFileConjunction.push_back(cardInCaseFileClause);
-        for(int other = 0; other <= num_suspects; other++) {
+        for(int other = 0; other < num_rooms; other++) {
             if(other != r) {
                 Clause cardNotInCaseFile;
                 cardNotInCaseFile.push_back(GetPairNum("cf", rooms[other]) * -1);
@@ -230,7 +230,7 @@ void ClueReasoner::AddInitialClauses()
     for(Clause clause : oneCardOfCategoryInCaseFile) {
         solver->AddClause(clause);
     }
-    oneCardOfCategoryInCaseFile.empty();
+    oneCardOfCategoryInCaseFile.clear();
 }
 
 
@@ -238,8 +238,11 @@ void ClueReasoner::Hand(string player, string cards[3])
 {
 	// GetPlayerNum returns the index of the player in the players array (not the suspects array). Remember that the players array is sorted wrt the order that the players play. Also note that, player_num (not to be confused with num_players) is a private variable of the ClueReasoner class that is initialized when this function is called.
 	player_num = GetPlayerNum(player);
-	
-	// TO BE IMPLEMENTED AS AN EXERCISE
+    for(int i = 0; i < 3; i++) {
+        Clause clause;
+        clause.push_back(GetPairNum(player, cards[i]));
+        solver->AddClause(clause);
+    }
 }
 
 void ClueReasoner::Suggest(string suggester, string card1, string card2, string card3, string refuter, string card_shown)
@@ -248,7 +251,54 @@ void ClueReasoner::Suggest(string suggester, string card1, string card2, string 
 	// In this C++ implementation, NULL is translated to be the empty string "".
 	// To check if refuter is NULL or card_shown is NULL, you should use if(refuter == "") or if(card_shown == ""), respectively.
 	
-	// TO BE IMPLEMENTED AS AN EXERCISE
+    if(refuter == "") {
+        //no refuter at all --> all players except suggester do not have suggested cards
+        for(int i = 0; i < num_players; i++) {
+            if(players[i] != suggester) {
+                Clause clause1;
+                clause1.push_back(GetPairNum(players[i], card1) * -1);
+                solver->AddClause(clause1);
+                Clause clause2;
+                clause2.push_back(GetPairNum(players[i], card2) * -1);
+                solver->AddClause(clause2);
+                Clause clause3;
+                clause3.push_back(GetPairNum(players[i], card3) * -1);
+                solver->AddClause(clause3);
+            }
+        }
+    }
+    else {
+        int refuterIndex = (GetPlayerNum(suggester) == (num_players - 1)) ? 0 : GetPlayerNum(suggester) + 1;
+        int lastRefuterIndex = GetPlayerNum(refuter);
+        while(refuterIndex != lastRefuterIndex) {
+            //The players in between do not have the cards
+            Clause clause1;
+            clause1.push_back(GetPairNum(players[refuterIndex], card1) * -1);
+            solver->AddClause(clause1);
+            Clause clause2;
+            clause2.push_back(GetPairNum(players[refuterIndex], card2) * -1);
+            solver->AddClause(clause2);
+            Clause clause3;
+            clause3.push_back(GetPairNum(players[refuterIndex], card3) * -1);
+            solver->AddClause(clause3);
+            refuterIndex = (refuterIndex == (num_players - 1)) ? 0 : refuterIndex + 1;
+        }
+        
+        //we reach the refuter. In this case, we either can see the card shown or not
+        if(card_shown == "") {
+            //player does not get to see which card was shown upon refute
+            Clause clause1;
+            clause1.push_back(GetPairNum(players[refuterIndex], card1));
+            clause1.push_back(GetPairNum(players[refuterIndex], card2));
+            clause1.push_back(GetPairNum(players[refuterIndex], card3));
+            solver->AddClause(clause1);
+        } else {
+            //player gets to see which card was shown
+            Clause clause1;
+            clause1.push_back(GetPairNum(players[refuterIndex], card_shown));
+            solver->AddClause(clause1);
+        }
+    }
 }
 
 void ClueReasoner::Accuse(string suggester, string card1, string card2, string card3, bool is_correct)
